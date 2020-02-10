5d7ba5b7e93e8ee290f3810b732b8d7965281e00
Fan_Li <victorlifan@vip.qq.com>
2020-02-09 22:49:24 -0600

    add comment on main function

--- bikeshare.py
+++ bikeshare.py
@@ -188,7 +188,7 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
+# main function
 def main():
     while True:
         city, month, day = get_filters()
